e6a0233896af79b54726d701c1675559da254c87
austin <austingarrett2k4@gmail.com>
2024-03-07 21:21:25 -0500

    BlitzApp

---
+++ src/displayapp/screens/BlitzApp.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "displayapp/Apps.h"
+#include "displayapp/screens/Screen.h"
+#include "displayapp/Controllers.h"
+#include "Symbols.h"
+
+namespace Pinetime {
+  namespace Applications {
+    namespace Screens {
+      class BlitzApp : public Screen {
+      public:
+        BlitzApp();
+        ~BlitzApp() override;
+      };
+    }
+    
+    template <>
+    struct AppTraits<Apps:BlitzApp> {
+      static constexpr Apps app = Apps::BlitzApp;
+      static constexpr const char* icon = Screens::Symbol::BlitzApp;
+      static Screens::Screens* Create(AppController& controllers) {
+        return new Screens::BlitzApp();
+      }
+    };
+  }
+}
